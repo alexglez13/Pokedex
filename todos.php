89df1817905fe55858ec1a35bb8c36b1209741e6
alexglez13 <alex.glez19@outlook.com>
2026-03-16 17:31:03 -0600

    Agregar mas pokemon

--- todos.php
+++ todos.php
@@ -1,7 +1,17 @@
 <?php
 include 'include/conect.php';
-$consulta = "SELECT * FROM pokemon";
-$consultatipo = "SELECT * FROM tipopoke";
+$consulta = "SELECT 
+pokemon.*, 
+tipopoke.ntpoke,
+sexo.nsexo,
+region.nregion,
+habilidad.nhabilidad
+FROM pokemon
+INNER JOIN tipopoke ON pokemon.id_tpoke = tipopoke.id_tpoke
+INNER JOIN sexo ON pokemon.id_sexo = sexo.id_sexo
+INNER JOIN region ON pokemon.id_region = region.id_region
+INNER JOIN habilidad ON tipopoke.id_habilidad = habilidad.id_habilidad
+ORDER BY pokemon.id_poke ASC";
 $resultado = $conexion->query($consulta);
 ?>
 
@@ -18,8 +28,36 @@ $resultado = $conexion->query($consulta);
 <h1 class="text-center mb-5">Todos los Pokémon</h1>
 <div class="row g-4">
 <?php while($row = $resultado->fetch_assoc()){ ?>
+<?php
+switch($row['ntpoke']){
+    case "Agua": $colorTipo = "bg-primary"; break;
+    case "Fuego": $colorTipo = "bg-danger"; break;
+    case "Planta": $colorTipo = "bg-success"; break;
+    default: $colorTipo = "bg-secondary";
+}
+?>
+<?php
+$fondoTipo = "";
+
+switch($row['ntpoke']){
+    case "Agua":
+        $fondoTipo = "bg-primary text-white";
+        break;
+
+    case "Fuego":
+        $fondoTipo = "bg-danger text-white";
+        break;
+
+    case "Planta":
+        $fondoTipo = "bg-success text-white";
+        break;
+
+    default:
+        $fondoTipo = "bg-light";
+}
+?>
 <div class="col-12 col-md-6 col-lg-4">
-<div class="card shadow h-100 text-center">
+<div class="card shadow h-100 text-center <?php echo $fondoTipo; ?>">
 <h5 class="card-title mt-4 text-uppercase text-center">
 <span class="fs-10 fw-bold">
 <?php echo $row['npoke']; ?>
@@ -30,7 +68,7 @@ class="card-img-top mt-2 p-3"
 style="height:350px; object-fit:contain;">
 <div class="card-body">
 <button type="button" 
-class="btn btn-primary mt-2" 
+class="btn btn-outline-light mt-2" 
 data-bs-toggle="modal" 
 data-bs-target="#modal<?php echo $row['id_poke']; ?>">
 Conoce Más
@@ -67,6 +105,17 @@ Conoce Más
 </p>
 <p class="mt-1">
 <?php echo "Altura ".$row['altura']." mts"; ?>
+</p>
+<p class="badge <?php echo $colorTipo; ?> fs-6">Tipo: <?php echo $row['ntpoke']; ?>
+</p>
+<p>
+Sexo: <?php echo $row['nsexo']; ?>
+</p>
+<p>
+Región: <?php echo $row['nregion']; ?>
+</p>
+<p>
+Habilidad: <?php echo $row['nhabilidad']; ?>
 </p>
 
 </div>
