445a99e399a7eda8ab640a91967834472f887d30
alexglez13 <alex.glez19@outlook.com>
2026-03-16 01:57:12 -0600

    cambios

--- fuego.php
+++ fuego.php
@@ -1,6 +1,6 @@
 <?php
 include 'include/conect.php';
-$consulta = "SELECT * FROM pokemon LIMIT 3";
+$consulta = "SELECT * FROM pokemon";
 $resultado = $conexion->query($consulta);
 ?>
 
@@ -11,35 +11,36 @@ $resultado = $conexion->query($consulta);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tipos de Pokémon</title>
     <link rel="stylesheet" href="assets/css/fuego1.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
 </head>
 <body>
     <!--titulo-->
-    <h1 class="titulo">Tipos de Pokémon</h1>
+    <h1 class="titulo">Todos los Pokémon</h1>
    
     <!--contenedor-->
-    
     <div class="contenedor">
-        <div class="tarjeta">
-           <?php
-    echo $alerta;
-    ?>
-    <?php while($row = $resultado->fetch_assoc()){ ?>
-    
-    <div class="pokemon">
-        <h3><?php echo $row['npoke']; ?></h3>
 
-        <img src="assets/img/<?php echo $row['imagen']; ?>" width="150">
+<?php while($row = $resultado->fetch_assoc()){ ?>
 
-        <p><?php echo $row['descripcion']; ?></p>
+    <div class="tarjeta">
+
+        <div class="pokemon">
+            <h3><?php echo $row['npoke']; ?></h3>
+
+            <img src="assets/img/<?php echo $row['imagen']; ?>" width="150">
+
+            <p><?php echo $row['descripcion']; ?></p>
+        </div>
+
+       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Charmander">
+            Conoce Mas
+        </button>
+
     </div>
 
 <?php } ?>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Charmander">
-            Conoce Mas
-</button>
-        </div>
-    </div>
+
+</div>
     <!-- modal-->
      <div class="modal fade" id="Charmander" tabindex="-1" aria-labelledby="CharmanderLabel" aria-hidden="true">
   <div class="modal-dialog  modal-dialog-centered">
