7026c2ba46955dde11b635463817ef0e437113d0
alexglez13 <alex.glez19@outlook.com>
2026-03-26 12:31:05 -0600

    agrega navbar

--- control.php
+++ control.php
@@ -51,28 +51,7 @@ if (isset($_GET['actualizacion']) && $_GET['actualizacion'] == "exitosa") {
 
 <body>
     <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="index.html">POKEDEX</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="menu">
-                <ul class="navbar-nav ms-auto">
-
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.html">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="prueba.php">Registrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="control.php">Control</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+   <?php include 'vistas/sidebar.php';?>
     <!--contenedor -->
     <div class="container">
         <h1 class="text-center mt-4">Consulta pokemon</h1>
