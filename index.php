56f5d6f75065af0ee288c48152c111b8c53b3a33
alexglez13 <alex.glez19@outlook.com>
2026-03-29 07:45:47 -0600

    Limpiar codigo

--- index.php
+++ index.php
@@ -11,28 +11,19 @@
 </head>
 
 <body>
-
     <?php include 'vistas/sidebar.php'; ?>
-
     <div class="container contenido d-flex flex-column justify-content-center align-items-center text-center">
-
         <img src="assets/img/upscalemedia-transformed-Photoroom (1).png"
              class="pokemon-hero mt-3" />
-
         <h1 class="display-3 fw-bold mt-2">¡Bienvenidos al mundo Pokémon!</h1>
-
         <p class="lead mt-2">
             Podrás conocer más sobre tus personajes favoritos
         </p>
-
         <a href="todos.php" class="btn btn-danger btn-custom mt-2">
             TODOS LOS POKEMON
         </a>
-
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="assets/js/bootstrap.bundle.min"></script>
 </body>
 
 </html>
