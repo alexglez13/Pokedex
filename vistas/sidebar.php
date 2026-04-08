177cd855139c4a8e520b9e46ea83443a74c3460c
Liliana-2109 <lilianaprospero613@gmail.com>
2026-04-07 22:30:59 -0600

    actializacionde sidebaer

--- vistas/sidebar.php
+++ vistas/sidebar.php
@@ -3,7 +3,8 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-         <a href="javascript:history.back()" class="btn btn-outline-light btn-sm">← </a>
+         <a href="javascript:history.back()" class="btn btn-outline-light btn-sm">
+← </a>
         <a class="navbar-brand fw-bold" href="index.php">POKEDEX</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
             <span class="navbar-toggler-icon"></span>
@@ -11,7 +12,8 @@
 
         <div class="collapse navbar-collapse" id="menu">
             <ul class="navbar-nav ms-auto align-items-center">
-  <!-- Inicio siempre visible -->
+
+                <!-- Inicio siempre visible -->
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">Inicio</a>
                 </li>
@@ -32,18 +34,37 @@
                         <a class="nav-link text-warning fw-bold" href="control.php">Control</a>
                     </li>
                 <?php endif; ?>
+
+
+                <?php if (isset($_SESSION['id_usuario'])): ?>
+
+    <?php if ($_SESSION['rol'] === 'editor'): ?>
+        <li class="nav-item ms-2">
+            <a class="nav-link" href="perfil.php?id_usuario=<?php echo $_SESSION['id_usuario'] ?>">
+                 Editor <?php echo htmlspecialchars($_SESSION['nombre']); ?>
+            </a>
+        </li>
+    <?php endif; ?>
+
+    <?php if ($_SESSION['rol'] === 'visitante'): ?>
+        <li class="nav-item ms-2">
+            <a class="nav-link" href="puntuacion.php?id_usuario=<?php echo $_SESSION['id_usuario'] ?>">
+                 Visitante <?php echo htmlspecialchars($_SESSION['nombre']); ?>
+            </a>
+        </li>
+    <?php endif; ?>
+
+<?php endif; ?>
+
                 <?php if (isset($_SESSION['id_usuario'])): ?>
                     <!-- Sesión activa: mostrar nombre y botón cerrar sesión -->
-                    <li class="nav-item ms-2">
-                        <a class="nav-link" href="perfil.php?id_usuario=<?php echo $_SESSION['id_usuario'] ?>">Bienvenid@ <?php echo htmlspecialchars($_SESSION['nombre']); ?></a>
-                    </li>
                     <li class="nav-item">
                         <a class="btn btn-outline-danger btn-sm ms-1" href="logout.php">Salir</a>
                     </li>
                 <?php else: ?>
 
                     <li class="nav-item ms-2">
-                        <a class="btn btn-danger btn-sm" href="registrarus.php">Registrar</a>
+                        <a class="btn btn-danger btn-sm" href="registrar.php">Registrar</a>
                     </li>
                     <!-- Sin sesión: mostrar botón login -->
                     <li class="nav-item ms-2">
